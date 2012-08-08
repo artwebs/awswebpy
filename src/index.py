2a10cfc7a3e63fb02e4ba457bfe3bae88201e0cb
artwebs <artwebs@126.com>
2012-08-08 16:48:41 +0800

    尝试修改

--- src/index.py
+++ src/index.py
@@ -3,6 +3,7 @@ from depend.bottle import Bottle,debug, run
 
 from action import root
 
+
 if __name__ == '__main__':
     debug(True)    
     run(root, host="0.0.0.0", port=8081, reloader=True)    
